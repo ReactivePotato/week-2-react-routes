1209ab66cabce4c1588b45b6c05413c2a2641349
ReactivePotato <potat@potatPC>
2021-10-17 02:33:27 +1000

    updated profile.js

--- client/components/profile.js
+++ client/components/profile.js
@@ -10,7 +10,7 @@ const Profile = () => {
       <div className="flex items-center justify-center h-screen">
         <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
           <div id="title">Profile</div>
-            y<div>
+            <div>
               <Link to="/dashboard">Go To Root</Link>
             </div>
             <div>
